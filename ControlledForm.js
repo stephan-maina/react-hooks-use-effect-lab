27da7599b7d9fb4e860f45bc483b16beadb8eac8
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-07-30 01:28:37 +0300

    Add files via upload

---
+++ ControlledForm.js
@@ -0,0 +1,12 @@
+<form>
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" />
+
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email" />
+
+  <label for="password">Password:</label>
+  <input type="password" id="password" name="password" />
+
+  <input type="submit" value="Submit" />
+</form>
